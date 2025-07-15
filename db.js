3a14a528003eca95b578f1fdd9cf16d3db5a0c6a
VaibhavRajai <rajaivaibhav28@gmail.com>
2025-07-15 16:43:02 +0530

    changes in create event api

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const {Pool} =require('pg')
 require('dotenv').config()
 const pool=new Pool({
-    connectionString:process.env.DATABASE_URL
+    connectionString:process.env.DATABASE_URL,  ssl: {
+    rejectUnauthorized: false, 
+  },
 })
 pool.connect()
   .then(() => console.log('PostgreSQL connected successfully!'))
